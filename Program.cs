6cd02159a38e9ba7aec9c0dd349e177a9d58e83c
DD2476 <DragosDragos2476@gmail.com>
2021-11-02 18:46:32 +0200

    Added tutorial

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿//README - DO NOT EDIT AT ANY POINT
+
+//Thanks for downloading my project!
+//Made by the doggo lmao.
+
+//For a little tutorial on how to add commands, go to line 73.
+//This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+//Removing the credits or hiding them is strictly prohibited.
+//The project requires the [Properties.Settings.Default] so please don't delete them. If you want to paste the code, create the settings or remove the code that uses them (not recommended).
+//It also requires [using System;] and [using static System.Console;], so if you want to remove the uses, make sure to keep these two.
+//If you suspect you have an unofficial version, you may download the official one here: https://dd2476ste1.7m.pl/projects/CMDSpoof/
+//The GitHub repo for this is here: https://github.com/DD2476/CMDSpoofCS/
+
+using System;
 using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
@@ -57,6 +70,20 @@ namespace CMDSpoofcs
             } else if (inp == "clear") {
                 Clear();
                 Main();
+                //TUTORIAL COMMANDS
+            } else if (inp == "example1") { //1 - If the input equals to a string
+                WriteLine("Example command. (1)");
+                WriteLine("");
+            } else if (inp.StartsWith("example2")) { //2 - If the input starts with a string
+                WriteLine("Example command. (2)");
+                WriteLine("");
+            } else if (inp.EndsWith("example3")) { //3 - If the input ends with a string
+                WriteLine("Example command. (3)");
+                WriteLine("");
+            } else if (inp.Contains("example4")) { //4 - If the input contains a string
+                WriteLine("Example command. (4)");
+                WriteLine("");
+                //END TUTORIAL COMMANDS
             } else {
                 WriteLine("'" + inp + "' is not recognized as an internal or external command,\noperable program or batch file.");
                 WriteLine("");
